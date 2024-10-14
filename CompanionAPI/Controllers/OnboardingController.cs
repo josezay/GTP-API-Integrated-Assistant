3e662cba4cde2ed5d47b5ad237dcd9052e026a58
Jose Zay <josezay22@gmail.com>
2024-10-13 21:30:06 -0300

    Add onboarding controller and update Swashbuckle packages
    Updated `CompanionAPI.csproj`:
    - Changed `Swashbuckle.AspNetCore` version from `6.4.0` to `6.8.1`
    - Added `Swashbuckle.AspNetCore.Annotations` version `6.8.1`
    - Added `Swashbuckle.Examples` version `4.1.0`
    
    Added `OnboardingController` in `OnboardingController.cs`:
    - POST endpoint `/api/onboarding/onboard` for user registration
    - Utilizes Swagger annotations for API documentation
    
    Added `UserOnboardingRequest` in `UserOnboardingRequest.cs`:
    - Defines request body structure for user onboarding
    - Includes properties: `Name`, `Gender`, `Age`, `Weight`, `Height`, `Goal`

---
+++ CompanionAPI/Controllers/OnboardingController.cs
@@ -0,0 +1,21 @@
+﻿using CompanionAPI.Requests;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CompanionAPI.Controllers;
+
+[Route("api/[controller]")]
+public class OnboardingController : ApiController
+{
+    [HttpPost]
+    [Route("onboard")]
+    [SwaggerOperation(Summary = "Register a new user", Description = "Registers a new user with the provided details.")]
+    [SwaggerResponse(200, "User registered successfully")]
+    [SwaggerResponse(400, "Invalid input data")]
+    public IActionResult OnboardUser([FromBody] UserOnboardingRequest request)
+    {
+
+
+        return Ok("User registered successfully");
+    }
+}
---
+++ CompanionAPI/Requests/UserOnboardingRequest.cs
@@ -0,0 +1,13 @@
+﻿using Swashbuckle.AspNetCore;
+
+namespace CompanionAPI.Requests;
+
+public class UserOnboardingRequest
+{
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+    public double Weight { get; set; }
+    public double Height { get; set; }
+    public string Goal { get; set; }
+}
