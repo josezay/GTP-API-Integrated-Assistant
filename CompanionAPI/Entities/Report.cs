a5fe03cd370714fc1f41d9f3136bf6108b297989
Jose Zay <josezay22@gmail.com>
2024-10-19 10:01:49 -0300

    Add Report class for Firestore integration
    Introduce a new `Report` class in `CompanionAPI.Entities` namespace, designed for use with Google Cloud Firestore. The class includes properties for query details, query time, result, and success status, all decorated with Firestore attributes. Added both a default and a private constructor for property initialization.

---
+++ CompanionAPI/Entities/Report.cs
@@ -0,0 +1,32 @@
+﻿using Google.Cloud.Firestore;
+
+namespace CompanionAPI.Entities;
+
+[FirestoreData]
+public class Report
+{
+    [FirestoreProperty]
+    public string Query { get; private set; }
+
+    [FirestoreProperty]
+    public DateTime QueriedAt { get; private set; }
+
+    [FirestoreProperty]
+    public string Result { get; private set; }
+
+    [FirestoreProperty]
+    public bool IsSuccessful { get; private set; }
+
+    public Report()
+    {
+    }
+
+    private Report(
+        string query,
+        DateTime queriedAt)
+    {
+        Query = query;
+        QueriedAt = queriedAt;
+    }
+
+}
